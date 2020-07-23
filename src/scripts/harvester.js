4ea31966dda3634782d4bcf42a1de13463f4a260
travisbrigman <travis1000@gmail.com>
2020-07-23 09:05:01 -0500

    working on Harverster module

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,3 +1,16 @@
-export const harvestPlants = () => {
-    return //arrayOfSeedObjects
+
+
+
+export const harvestPlants = (plantsArray) => {
+    const arrayOfSeedObjects = []
+    plantsArray.forEach(plant => {
+        if (plant.type !== "Corn") {
+            for (const i = 0;  i < plant.output; i++) {
+                return arrayOfSeedObjects.push(plant)
+            } 
+        } else {
+            return arrayOfSeedObjects.push(plant)
+        }
+    });
+    return arrayOfSeedObjects
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,13 @@
 import { plantSeeds } from "./tractor.js"
 import { addPlant, usePlants } from "./field.js"
 import { createPlan } from "./plan.js"
+import { harvestPlants } from "./harvester.js"
 
 const plantingPlan = createPlan()
 plantSeeds(plantingPlan)
 const plantedField = usePlants()
 console.log(plantedField)
 
+const harvestTime = harvestPlants(plantedField)
+
+console.log(harvestTime)
